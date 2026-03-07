096a1b1de57ec0bd93aa7b8da782aa424f210bcb
la .varik. .VALefor. <varikvalefor@aol.com>
2026-03-06 23:57:59 -0500

    ni'o mabla cikre pe'a

--- modbot.sh
+++ modbot.sh
@@ -3,11 +3,10 @@ set -x
 kibysehu=matrix.catgirl.cloud
 
 accessToken=$(head -n 1 $HOME/.config/modbot/accesstoken)
-kumfaId="$(cat ~/prgs/kumfaid)"
 
 echo $accessToken
 
-alias c="curl --socks5 127.0.0.1:9050 -H 'Authorization: Bearer $accessToken'"
+alias c="curl --socks5 127.0.0.1:9050 -H Authorization:\ Bearer\ \"${accessToken}\""
 
 function evtId {
 	evtId=$(c "https://$kibysehu/_matrix/client/v3/rooms/$1/state")
@@ -17,32 +16,39 @@ function evtId {
 function ningau {
 	kumfaId="$1"
 	evtx=$(c "https://$kibysehu/_matrix/client/v3/rooms/$1/state")
-	echo "$evtx"
+	#echo "$evtx"
 	evt=$(echo "$evtx" | jq '.[] | select(.type == "m.room.power_levels")') || echo "shit"
 	zbas=$(echo "$evtx" | jq '.[] | select(.type == "m.room.create") | .sender')
-	versiio=$(echo "$evtx" | jq '.[] | select(.type == "m.room.create") | .room_version')
+	versiio=$(echo "$evtx" | jq '.[] | select(.type == "m.room.create") | .content.room_version | tonumber')
+	echo $versiio
 	evtId="$(echo "$evt" | jq '.event_id')"
 
-	pilnoLv={$(cat ~/prgs/pwrlv_1 | perl -pe 's/^([^\s]*)/"\1":/; s/$/,/' | grep -v "$zbas" | sed -e '$s/,//')}
+	pilnoLv="{$(cat pwrlv_1 | perl -pe 's/^([^\s]*)/"\1":/; s/$/,/' | sed -e '$s/,//')}"
+	#echo $pilnoLv | jq
 
 	# ni'o lo so'i versiio co'e na mapti tu'a lo barda je mu'oi glibau. power level .glibau.
 	# .i fanta lo nu samfli
 	if [ $versiio -lt 12 ]
 	then
-		pilnoLv=$(echo "$pilnoLv" | jq 'map_values(if . > 100 then 100 else . end)')
+		pilnoLv=$(echo "$pilnoLv" | jq "map_values(if . > 100 then 100 else . end)")
+	else
+		pilnoLv=$(echo "$pilnoLv" | jq "del(.$zbas)")
 	fi
 
 	# debug crap
-	echo $pilnoLv
+	#echo $pilnoLv | jq
 
-	evt2=$(echo "$evt" | jq ".users |= $pilnoLv | del(.event_id,.room_id,.origin_server_ts,.sender)")
-	echo "$evt2" #| jq '.content.users'
+	evt2=$(echo "$evt" | jq ".users |= $pilnoLv | .content.users |= $pilnoLv | del(.event_id,.room_id,.origin_server_ts,.sender) | del(.unsigned,.prev_content)")
+	echo "$evt" | jq '.content.users'
+	echo "$evt2" | jq
+	echo "$evt" | jq '.content.users' | wc -l
+	echo "$evt2" | jq '.content.users' | wc -l
 
 	c -X PUT "https://$kibysehu/_matrix/client/v3/rooms/$kumfaId/state/m.room.power_levels" -d "$evt2"
 
 }
 
-for i in $(cat ~/prgs/kumfaid)
+for i in $(cat kumfaid)
 do
 	ningau $i
 done
