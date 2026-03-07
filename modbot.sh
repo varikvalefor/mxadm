f60834ad80b0d8680f42a0715b6d1a6dda0f6e23
la .varik. .VALefor. <varikvalefor@aol.com>
2026-03-07 00:19:17 -0500

    ni'o vimcu pe'a le me'oi .debug. co'e

--- modbot.sh
+++ modbot.sh
@@ -4,8 +4,6 @@ kibysehu=matrix.catgirl.cloud
 
 accessToken=$(head -n 1 $HOME/.config/modbot/accesstoken)
 
-echo $accessToken
-
 alias c="curl --socks5 127.0.0.1:9050 -H Authorization:\ Bearer\ \"${accessToken}\""
 
 function evtId {
@@ -16,15 +14,12 @@ function evtId {
 function ningau {
 	kumfaId="$1"
 	evtx=$(c "https://$kibysehu/_matrix/client/v3/rooms/$1/state")
-	#echo "$evtx"
 	evt=$(echo "$evtx" | jq '.[] | select(.type == "m.room.power_levels")') || echo "shit"
 	zbas=$(echo "$evtx" | jq '.[] | select(.type == "m.room.create") | .sender')
 	versiio=$(echo "$evtx" | jq '.[] | select(.type == "m.room.create") | .content.room_version | tonumber')
-	echo $versiio
 	evtId="$(echo "$evt" | jq '.event_id')"
 
 	pilnoLv="{$(cat pwrlv_1 | perl -pe 's/^([^\s]*)/"\1":/; s/$/,/' | sed -e '$s/,//')}"
-	#echo $pilnoLv | jq
 
 	# ni'o lo so'i versiio co'e na mapti tu'a lo barda je mu'oi glibau. power level .glibau.
 	# .i fanta lo nu samfli
@@ -35,14 +30,7 @@ function ningau {
 		pilnoLv=$(echo "$pilnoLv" | jq "del(.$zbas)")
 	fi
 
-	# debug crap
-	#echo $pilnoLv | jq
-
 	evt2=$(echo "$evt" | jq ".users |= $pilnoLv | .content.users |= $pilnoLv | del(.event_id,.room_id,.origin_server_ts,.sender) | del(.unsigned,.prev_content)")
-	echo "$evt" | jq '.content.users'
-	echo "$evt2" | jq
-	echo "$evt" | jq '.content.users' | wc -l
-	echo "$evt2" | jq '.content.users' | wc -l
 
 	c -X PUT "https://$kibysehu/_matrix/client/v3/rooms/$kumfaId/state/m.room.power_levels" -d "$evt2"
 
