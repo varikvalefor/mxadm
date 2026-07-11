144514c67273002f893a3c1c936655555d328b62
la .varik. .VALefor. <varikvalefor@aol.com>
2026-07-11 10:56:36 -0400

    ni'o feglyzvi

--- ticker2.sh
+++ ticker2.sh
@@ -7,18 +7,27 @@ set -x
 kibysehu=matrix.catgirl.cloud
 
 kumfaId_ticketSpc=$(cat $HOME/.config/modbot/xkumfaid_ticketspc)
+kumfaId_oTicketSpc=$(cat $HOME/.config/modbot/xkumfaid_oticketspc)
 kumfaId_jatna=$(cat $HOME/.config/modbot/xkumfaid_jatna)
 
 accessToken=$(head -n 1 $HOME/.config/modbot/accesstoken)
 
-#alias c="curl -x socks5h://10.255.1.3:4447 -H Authorization:\ Bearer\ \"${accessToken}\""
-alias c="curl -x http://10.255.1.3:4444 -H Authorization:\ Bearer\ \"${accessToken}\""
+#alias c="curl --retry 100 -x socks5h://10.255.1.3:9050 -H Authorization:\ Bearer\ \"${accessToken}\""
+alias c="curl --retry 100 -x http://10.255.1.3:4444 -H Authorization:\ Bearer\ \"${accessToken}\""
+
+function guido {
+        # | ni'o pilno ko'a goi la'o zoi. dd(1) .zoi. ki'u le
+        # su'u ko'a me'oi .lazy.
+        tr -dc A-Za-z0-9 < /dev/random | dd if=/dev/stdin of=/dev/stdout bs=1 "count=$1" status=none;
+        echo "";
+}
 
 function SYNC {
 	set -x
 
 	SINCE=$(cat ~/.config/modbot/since1)
-	evt=$(c "https://matrix.catgirl.cloud/_matrix/client/v3/sync?filter=4&since=$SINCE")
+	#evt=$(c "https://matrix.catgirl.cloud/_matrix/client/v3/sync?filter=4&since=$SINCE")
+	evt=$(c "https://matrix.catgirl.cloud/_matrix/client/v3/sync?filter=8&since=$SINCE")
 	nb=$(echo "$evt" | jq -r '.next_batch')
 	if [ "$nb" ]
 	then
@@ -26,12 +35,12 @@ function SYNC {
 	fi
 	echo "$evt"
 }
+		#jq -r 'if .body | test("^\\!mxadm ticket (moderator|verify|incident)$") then .body = (.body | match("\\w+$") | .string) + "|" + .sender else . end';
 
 function syncRq {
 	set -x
 	SYNC |\
-		jq -c '.rooms.join | to_entries[] | .value.timeline.events[] += {roomid: .key} | .value.timeline.events[] | select(.content.msgtype == "m.text") | . += {body: .content.body} | {sender, roomid, body}' |\
-		jq -r 'if .body | test("^\\!mxadm ticket (moderator|verify)$") then [(.body | match("\\w+$") | .string) + "|" + .sender] | .[] else "\(.)" end';
+		jq -c '.rooms.join | to_entries[]' | tee "$HOME/.syncs/$(date +%Y%m%d%H%M%S).sync" | jq -cr '.value.timeline.events[] += {roomid: .key} | .value.timeline.events[] | . += {body: .content.body}';# | {sender, roomid, body}';
 }
 
 function mkTik {
@@ -44,6 +53,9 @@ function mkTik {
 	if [ "$klesi" = "verify" ]
 	then
 		klesiX="Verification"
+	elif [ "$klesi" = "incident" ]
+	then	
+		klesiX="Incident"
 	else
 		klesiX="Moderator"
 	fi
@@ -65,8 +77,9 @@ function mkTik {
 	./modbot.sh over9000 "$kumfaId" cotf-tik1
 
 	c -X PUT "https://$kibysehu/_matrix/client/v3/rooms/$kumfaId_ticketSpc/state/m.space.child/$kumfaId" -d "{\"suggested\": \"false\", \"via\": [\"catgirl.cloud\"]}"
+	c -X PUT "https://$kibysehu/_matrix/client/v3/rooms/$kumfaId_oTicketSpc/state/m.space.child/$kumfaId" -d "{\"suggested\": \"false\", \"via\": [\"catgirl.cloud\"]}"
 
-	c -X PUT "https://$kibysehu/_matrix/client/v3/rooms/$kumfaId_jatna/send/m.room.message/$(guido 32)" -d "{\"msgtype\": \"m.text\", \"body\": \"new $klesi ticket for $mxid\nhttps://matrix.to/#/$kumfaId?via=catgirl.cloud\"}"
+	c -X PUT "https://$kibysehu/_matrix/client/v3/rooms/$kumfaId_jatna/send/m.room.message/$(guido 32)" -d "{\"msgtype\": \"m.text\", \"body\": \"new ($klesi ticket) for $mxid\nhttps://matrix.to/#/$kumfaId?via=catgirl.cloud\\ncotfNewTicketKeyword\"}"
 
 	if [ "$klesiX" = "Verification" ]
 	then
@@ -75,16 +88,16 @@ function mkTik {
 }
 
 function isTicketRequest {
-	echo "$1" | pcregrep '^\w+\|@.*:.+$'
+	echo "$1" | pcregrep "^\!mxadm ticket \w+$"
 }
 
 function slowmode {
         msg=$1
-	benji=$(echo "msg" | jq '.sender')
-	kumfaId=$(echo "msg" | jq '.room_id')
-	kumfaId=$(echo "msg" | jq '.origin_server_ts')
+	benji=$(echo "$msg" | jq '.sender')
+	kumfaId=$(echo "$msg" | jq '.room_id')
+	#kumfaId=$(echo "$msg" | jq '.origin_server_ts')
 
-        if (isPrivilegedUser "$(echo \"msg\" | jq '.sender')")
+        if (isPrivilegedUser "$(echo \"$msg\" | jq '.sender')")
         then
 		:;
 	else
@@ -145,24 +158,41 @@ function lupe {
 
 	sort -u | while read i
 	do
+		echo -E "$i"
+
 		set -x
-		bod=$(echo "$i" | jq -r '.body')
+		bod=$(echo -E "$i" | jq -r '.body')
+		kumfaId=$(echo -E "$i" | jq '.roomid')
+		klesi=$(echo "$i" | jq '.type')
 
 		#slowmode "$i"
 
-		if isTicketRequest "$i"
+		if true # [ "$kumfaId" = "!1_lk3HyvX3yeRwbGRyDNzsw6efoN6yoGJRk_YFUCrjo" ] || [ "$kumfaId" = "!utnlbJeRBwvqLUFltM:matrix.org" ]
 		then
-			klesi=$(echo "$i" | sed -e 's/|.*//g');
-			mxid=$(echo "$i" | sed -e 's/^.*|//');
+		echo "test pass"
+		if isTicketRequest "$bod"
+		then
+			klesi=$(echo -E "$bod" | sed -e 's/^.* //g');
+			mxid=$(echo "$i" | jq -r '.sender')
 			mkTik "$klesi" "$mxid";
+		elif echo "$klesi" | pcregrep "m\\.room\\.redaction" # .i la'oi .[. smimlu fi le ka ce'u co'e ja tolmapti  .i na jimpe fa la .varik.
+		then
+			set -x
+			notciId=$(echo "$i" | jq -rc '.content.redacts')
+			V=$(cat $HOME/.syncs/* | jq ".value.timeline.events[] | select(.event_id == \"$notciId\")") #base64 | perl -0777pe 's/\s//g')
+			if [ "$V" ]
+			then
+				V=$(jq -n --arg var "$V" "{\"msgtype\": \"m.text\", \"body\": \"ni'o vimcu pe'a lo notci  .i ku'i benji le velcki be le notci\n\nA message is \\\"deleted\\\".  But the definition of the event is sent.\n\n\" + \$var }")
+				c -X PUT "https://$kibysehu/_matrix/client/v3/rooms/!z9OfPbvrq1riCqwxqIQ9QEdttteNjSFTDswwneMEphE/send/m.room.message/$(guido 32)" -d "$V"
+			fi
+		fi
 		elif (echo "$bod" | grep -i 'sounds like a lot of$')
 		then
-			kumfaId=$(echo "$i" | jq -r '.roomid')
 			c -X PUT "https://$kibysehu/_matrix/client/v3/rooms/$kumfaId/send/m.room.message/$(guido 32)" -d "{\"msgtype\": \"m.text\", \"body\": \"HOOPLA!\"}"
 		elif [ "$bod" = "!mxadm help" ]
 		then
 			kumfaId=$(echo "$i" | jq -r '.roomid')
-			notci="!mxadm help - displays commands\\n!mxadm ticket moderator - creates moderator ticket\\n!mxadm divinationbyquote - outputs quote from varik's quote list\\n!mxadm [secret] - easter eggs and whatnot\\n\\n.i la .varik. cu kajde fi zo'e joi le su'u le proga cu co'e ja tolmapti lo se stika pe'a je notci\\n\\nVARIK cautions.  The bot is incompatible/whatever with messages which are \\\"edited\\\"."
+			notci="!mxadm help - displays commands\\n!mxadm ticket moderator - creates moderator ticket\\n!mxadm ticket verify - creates verification ticket\\n!mxadm ticket incident - creates incident ticket\\n!mxadm divinationbyquote - outputs quote from varik's quote list\\n!mxadm [secret] - easter eggs and whatnot\\n\\n.i la .varik. cu kajde fi zo'e joi le su'u le proga cu co'e ja tolmapti lo se stika pe'a je notci... je cu milxe le ka ce'u masno\\n\\nVARIK cautions.  The bot is incompatible/whatever with messages which are \\\"edited\\\".  Additionally, the bot is somewhat slow."
 			c -X PUT "https://$kibysehu/_matrix/client/v3/rooms/$kumfaId/send/m.room.message/$(guido 32)" -d "{\"msgtype\": \"m.text\", \"body\": \"$notci\"}"
 		elif [ "$bod" = "!mxadm slowmode enable" ]; then if isModerator "$(echo -E $i | jq -r '.sender')"
 		then
@@ -206,7 +236,7 @@ function lupe {
 #				c -X PUT "https://$kibysehu/_matrix/client/v3/rooms/$kumfaId/send/m.room.message/$(guido 32)" -d "{\"msgtype\": \"m.text\", \"body\": .i co'e ja djica lo nu cpedu lo se du'u ma kau krinu\\n\\nExplaining the reason is desired/whatever.\"}"
 #			fi
 #		fi
-		fi
+		fi # kumfaId
 	done
 }
 
@@ -223,6 +253,8 @@ function mane {
 			#slowmodeReset "fuck";
 		#done
 
-		sleep 60;
+		sleep 30;
 	done
 }
+
+mane
